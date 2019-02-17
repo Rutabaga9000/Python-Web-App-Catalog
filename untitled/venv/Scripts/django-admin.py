493df79b28691a9420c38ad7feeae27e20b304ab
Leonid <fokusnik229@gmail.com>
2019-02-17 20:17:22 +0400

    Hello World!
    Installed Python and Django
    Has been run a first site on a local server

---
+++ untitled/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!M:\Programming Projects\PycharmProjects\Python-Web-App-Catalog\untitled\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
