374c257940124bf1ed627541a75939102b1b74be
Leonid <fokusnik229@gmail.com>
2019-04-17 12:29:53 +0400

    Django tutorial End of Part 4
    forms and generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,6 +6,19 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: /polls/
+<<<<<<< HEAD
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
+    # added the word 'specifics'
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+=======
     path('', views.index, name='index'),
     # ex: /polls/5/
     # the 'name' value as called by the {% url %} template tag
@@ -18,3 +31,4 @@ urlpatterns = [
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
 ]
+>>>>>>> master
